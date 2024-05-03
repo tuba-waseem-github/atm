6973c28478b379821c9fbb6d6bbccea91df3ba64
tuba waseem <tubaw891@gmail.com>
2024-05-03 07:49:12 -0700

    Atm project

---
+++ main.ts
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer" ;
+ 
+let mybalance = 10000; // Dollar
+
+let myPin = 1234;
+
+let pinAnswere = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message: "enter your pin",
+            type: "number",
+        }
+       
+    ]
+);
+  //  12345 === 1234 = False
+
+if (pinAnswere.pin === myPin) {
+    console.log("correct pin code!!!");
+}
+
+   let operationAns= await inquirer.prompt(
+       [
+            {
+              name: "operations",
+              message: "please select one option",
+              type: "list",
+              choices: ["withDraw", "checkBalance"],
+            } 
+       ]
+
+   );
+console.log(operationAns);
+
+   if (operationAns.operations === "withDraw") {
+   let amountAns = await inquirer.prompt(
+    [
+        {
+            name: "amount",
+            message: "enter your amount",
+            type: "number",
+        }
+    ]
+   );
+   // =, -= , +=
+   mybalance -= amountAns.amount;
+   console.log("my remaining balance is: "+ mybalance);
+
+} else if (operationAns.operations === "checkBalance") {
+    console.log("Your balance is : " + mybalance);
+};
+
+
+ 
+
